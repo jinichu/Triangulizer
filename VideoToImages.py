0a03eef6a4a6607612d40d8434af4125fe02a9db
TylerJamesMalloy <tylerjmalloy@gmail.com>
2017-03-19 03:07:13 -0700

    commit 2

--- VideoToImages.py
+++ VideoToImages.py
@@ -9,8 +9,6 @@ import operator
 from multiprocessing import Pool
 from triangles.delaunay import triangularize
 from scipy.misc import imsave
-from Naked.toolshed.shell import execute_js, muterun_js
-from mapreduce import MapReduce, Frame
 imageio.plugins.ffmpeg.download()
 
 def triangularize_frame(frame):
@@ -22,7 +20,7 @@ def triangularize_frame(frame):
         imsave(frameFileName,fr)
 
         # convert img to triangle version
-        return (timeout(triangularize, args=(absolute_path,100), timeout_duration=2), idx)
+        return (timeout(triangularize, args=(absolute_path,1500), timeout_duration=20), idx)
     else:
         return (None,idx) 
 
@@ -55,8 +53,8 @@ def getVideo(InputFileName):
     fps = vid.get_meta_data()['fps']
     FullFilePath = InputFileName[0:len(InputFileName) - 4] + '_triangular' + InputFileName[len(InputFileName) - 4: len(InputFileName)]
     #writer = imageio.get_writer('/Users/tyler/Desktop/NWhacks/TriVideo/tmp/processed_Fireworks.mp4', fps=fps)
-    slowerfps = fps/4
-    writer = imageio.get_writer(FullFilePath, fps=slowerfps)
+    #slowerfps = fps/4
+    writer = imageio.get_writer(FullFilePath, fps=fps)
     p = Pool(8)
     frames = p.map(triangularize_frame, zip(vid, range(len(vid))))
     sorted_frames = sorted(frames, key=operator.itemgetter(1))
@@ -64,30 +62,6 @@ def getVideo(InputFileName):
         if fr is not None:
             processedImage = imageio.imread(fr)
             writer.append_data(processedImage)
-
-    # j = 5
-    # for im in vid:
-    #     j = j + 1
-    #     if j % 5 != 0:
-    #             continue 
-    #     if j < 5000 and j > 2000:
-    #         filename = 'Frames/Frame%d.jpg' % j
-    #         absolute_path = '/Users/tyler/Desktop/NWhacks/TriVideo/' + filename 
-    #         #filtered_im  = gaussian(im, sigma=0.4)
-    #         filtered_im = im
-    #         imsave(filename,filtered_im)
-    #         # trigfile = triangularize(absolute_path,5000)
-    #         trigfile = timeout(triangularize, args=(absolute_path,3000), timeout_duration=20)
-    #         if trigfile is None:
-    #             continue
-    #         else:
-    #             ### process image here
-    #             processedImage = imageio.imread(trigfile)
-    #             #filtered_im = processedImage.filter(ImageFilter.BLUR)
-    #             filtered_im = processedImage
-    #             # processedImage = imageio.imread(filename)
-    #             writer.append_data(filtered_im)
-    # writer.close()
     return FullFilePath
 
-getVideo('/Users/tyler/Desktop/NWhacks/TriVideo/tmp/Fireworks.mp4')
+getVideo('/Users/tyler/Desktop/NWhacks/TriVideo/tmp/Waves.mp4')
